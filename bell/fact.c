01b6f14ed45d8fd487e93e26aeb9932b089a8a29
tiokang <frankbellking@mail.com>
2018-08-07 11:07:03 +0100

    commit myfactorial

---
+++ bell/fact.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+int fact(int fact, int n){
+}
+if(n==0){
+
+return 0;
+}
+
+if(n==1){
+return 1;
+}
+
+else{
+return n*fact(n-1);
+
+}
+int main(){
+int num =0;
+printf("enter a num\n");
+
+scanf("%d\n",&n);
+int ans =fact(num)
+printf("fact of %d",fact);
+
+return 0;
+}
+
---
+++ bell/factorial.c
@@ -0,0 +1,25 @@
+#incllude <stdio.h>
+int fact(int n)
+{
+if(n==0)
+{
+return 0;
+}
+if(n==1)
+{
+return 1;
+}
+else
+{
+return n*fact(n-1);
+int main()
+{
+int num=0;
+printf("enter a number");
+scanf("%d",&n);
+int ans = fact(num)
+printf("factorialof %d",ans);
+return 0;
+  }
+   }
+}
